fe9a3a1b4b2c9bb34d1afdbb0faac2133a248ee6
superzdd <476257993@qq.com>
2024-01-17 00:04:25 +0800

    add assets

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -15,13 +15,29 @@ const { profile } = sourceData
       </h1>
     </div>
     <div>
-      <p>Hi，你好，我是张辰海</p>
+      <p>Hi！你好，我是张辰海</p>
       <p>职业是一名前端开发工程师</p>
-      <p>曾先后就职于</p>
-      <p>万达信息股份有限公司</p>
-      <p>大连东软思维科技发展有限公司上海分公司</p>
-      <p>上海亨兹瑞信息科技有限公司</p>
-      <p>博世华域转向系统有限公司</p>
+
+      <br>
+      <p>曾先后就职于：</p>
+      <ul>
+        <li>万达信息股份有限公司</li>
+        <li>大连东软思维科技发展有限公司上海分公司</li>
+        <li>上海亨兹瑞信息科技有限公司</li>
+        <li>博世华域转向系统有限公司</li>
+      </ul>
+
+      <br>
+
+      <div class="icon-neusoft" />
+      <img src="/imgs/herdsric-logo.jpg" alt="herdsric">
+
+      <p>从事程序员和项目经理的工作。在上海虹桥塔台，复星，阿里巴巴，还有上汽的整车中都留下过我工作的痕迹。</p>
+
+      <br>
+      <p>我目前关注前端领域，熟悉原生和Vue框架，今后会不断探索Stable Diffusion以及机器学习的相关技术和项目</p>
+
+      <p>我平时的兴趣是运动，看书和打游戏。同时我是NBA，任天堂的超级粉丝，如果你也这些方面的话题，我会非常高兴和你聊天。</p>
 
       <p>
         Find me on
@@ -32,15 +48,15 @@ const { profile } = sourceData
         </AppLink>
       </p>
 
-      <!-- <Counter :initial="1" /> -->
-
-      <!-- <a i-simple-icons-github op75 href="https://github.com/superzdd" target="_blank">GitHub</a> -->
-      <!-- <p>2010年从上海海事大学毕业后，</p>
-      <p>2010-2015，从事c#开发windows桌面程序</p>
-      <p>2015-2021，从事web前端,开发移动端html5</p>
-      <p>2021-2023，从事项目经理，但发现自己不擅长，所以决心回到技术中来</p> -->
-
       <br>
     </div>
   </div>
 </template>
+
+<style>
+.icon-neusoft {
+  background-image: url('/imgs/neusoft-logo.svg');
+  width: 100px;
+  height: 100px;
+}
+</style>
